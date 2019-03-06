8100da84f6a13cdbe95c25e977bec81287d9a62a
Yuma Sumi <rbly.776324.yuma66krayt@gmail.com>
2019-03-06 18:38:29 +0900

    added JSON parser in OpenFile.cs

---
+++ ego-trip/Assets/Scripts/AdsInterest.cs
@@ -0,0 +1,14 @@
+﻿public class AdsInterest
+{
+    public string[] topics;
+
+    public string toString() 
+    {
+        string output = "";
+        for (int i = 0; i < topics.Length; i ++ )
+        {
+            output += topics[i] + "\n";
+        }
+        return output;
+    }
+}
