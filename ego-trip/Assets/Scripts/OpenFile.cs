67ea1e161ad2cd358ceb1b604a8e3068f345622f
dejaviewdujour <ianismc@gmail.com>
2019-03-06 16:26:21 +0900

    added working json display

--- ego-trip/Assets/Scripts/OpenFile.cs
+++ ego-trip/Assets/Scripts/OpenFile.cs
@@ -6,6 +6,7 @@ using UnityEditor;
 
 public class OpenFile : MonoBehaviour
 {
+    string data = "JSON goes here";
    [MenuItem("Example/Overwrite Texture")]
    public void OnClick()
    {
@@ -15,10 +16,14 @@ public class OpenFile : MonoBehaviour
        Debug.Log(path);
        if (path.Length != 0)
        {
-           var fileContent = File.ReadAllText(path);
+           string fileContent = File.ReadAllText(path);
            Debug.Log(fileContent);
-           Debug.Log(JsonUtility.FromJson<string>(fileContent));
+           data = fileContent;
+        //    data = JsonUtility.FromJson<string>(fileContent);
+        //    Debug.Log(data);
        }
-
    }
+    void OnGUI() {
+        GUI.Box(new Rect(100, 100, 300, int.MaxValue), data);
+      }
 }
