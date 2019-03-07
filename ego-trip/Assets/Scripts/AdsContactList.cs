e8b4f40fb72adaa4d63a353535a6e474ec402a50
toshyss <toshy.ss@gmail.com>
2019-03-07 12:11:52 +0900

    Creating managing json for contact list

---
+++ ego-trip/Assets/Scripts/AdsContactList.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+public class AdsContactList
+{
+    public string[] custom_audiences;
+
+    public string toString()
+    {
+        string output = "";
+        for (int i = 0; i < custom_audiences.Length; i++)
+        {
+            output += Encoding.UTF8.GetString(Encoding.GetEncoding("iso-8859-1").GetBytes(custom_audiences[i])) + "\n";
+        }
+        return output;
+    }
+}
