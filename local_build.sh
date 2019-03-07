9bb30b83838f902caa53abb718ce7ceb8cc1e415
zero4994 <cavc94@gmail.com>
2019-03-07 15:38:15 +0900

    Adds configuration files

---
+++ local_build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+set -x
+
+export UNITY_EXECUTABLE=${UNITY_EXECUTABLE:-"/Applications/Unity/Hub/Editor/2018.3.7f1/Unity.app/Contents/MacOS/Unity"}
+export BUILD_NAME=${BUILD_NAME:-"ExampleProjectName"}
+
+BUILD_TARGET=StandaloneLinux64 ./ci/build.sh
+BUILD_TARGET=StandaloneOSX ./ci/build.sh
+BUILD_TARGET=StandaloneWindows64 ./ci/build.sh
+BUILD_TARGET=WebGL ./ci/build.sh
+
