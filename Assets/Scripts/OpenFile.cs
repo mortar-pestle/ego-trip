5c77509279a1dfb227ea2102fb0295765ee86551
zero4994 <cavc94@gmail.com>
2019-03-11 11:27:27 +0900

    Adds functionality to open folder using a key

--- Assets/Scripts/OpenFile.cs
+++ Assets/Scripts/OpenFile.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 using System.IO;
 using SFB;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
-using System.Text;
 using UnityEngine.UI;
 public class OpenFile : MonoBehaviour
 {
@@ -18,31 +13,22 @@ public class OpenFile : MonoBehaviour
     private string _path;
 
     public void OnClick()
-   {
+    {
 
 
         Debug.Log("clicked");
-        string folderPath = "";
-
-        var paths = StandaloneFileBrowser.OpenFolderPanel("Select Folder", "", false);
-
-        if (paths.Length == 0)
-        {
-            return;
-        }
-
-        folderPath = paths[0];
+        string folderPath = _path;
 
         Debug.Log(folderPath);
 
-       string filePath = Path.Combine(folderPath + "/ads/advertisers_who_uploaded_a_contact_list_with_your_information.json");
+        string filePath = Path.Combine(folderPath + "/ads/advertisers_who_uploaded_a_contact_list_with_your_information.json");
 
 
-       Debug.Log(filePath);
-       if (filePath != null && filePath.Length != 0)
-       {
-           string fileContent = File.ReadAllText(filePath);
-           Debug.Log(fileContent);
+        Debug.Log(filePath);
+        if (filePath != null && filePath.Length != 0)
+        {
+            string fileContent = File.ReadAllText(filePath);
+            Debug.Log(fileContent);
 
             //data = fileContent;
             dataAsJson = JsonUtility.FromJson<AdsContactList>(fileContent);
@@ -51,11 +37,36 @@ public class OpenFile : MonoBehaviour
             dataAsString = dataAsJson.custom_audiences.Length + " businesses have your personal information.\n" + dataAsString;
             Debug.Log(dataAsJson.custom_audiences.Length);
             info.text = dataAsString;
-       } 
-       else
-       {
+        }
+        else
+        {
             dataAsString = "File not found! Please navigate to the right directory.";
             Debug.LogError(dataAsString);
-       }
+        }
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Space(20);
+        GUILayout.BeginHorizontal();
+        GUILayout.Space(20);
+        GUILayout.BeginVertical();
+
+        if (GUILayout.Button("Open File Directory") || Input.GetKeyDown("o"))
+        {
+            var paths = StandaloneFileBrowser.OpenFolderPanel("Select Folder", "", false);
+
+            if (paths.Length == 0)
+            {
+                return;
+            }
+            _path = paths[0];
+            OnClick();
+        }
+
+        GUILayout.EndVertical();
+        GUILayout.Space(20);
+        GUILayout.Label(_path);
+        GUILayout.EndHorizontal();
     }
 }
