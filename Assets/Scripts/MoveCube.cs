b444e43172956fd735ca8b3e9543e9d152129e00
toshyss <toshy.ss@gmail.com>
2019-03-15 16:21:23 +0900

    imported sample panel to master

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Vector3 moveTo;
+    Camera camera;
+
+    private bool beRay = false;
+
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            RayCheck();
+        }
+
+        if(beRay)
+        {
+            MovePosition();
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            beRay = false;
+        }
+    }
+
+    private void RayCheck()
+    {
+        Ray ray = new Ray();
+        RaycastHit hit = new RaycastHit();
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity) && hit.collider == gameObject.GetComponent<Collider>())
+        {
+            beRay = true;
+        } else
+        {
+            beRay = false;
+        }
+    }
+
+    private void MovePosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+
+        moveTo = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = moveTo;
+    }
+}
